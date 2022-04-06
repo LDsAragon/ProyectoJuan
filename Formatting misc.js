f487187a1a43b37b4bf4f16dd0d1f583571bc2fe
Matias Salomone <matisalomone@hotmail.com>
2022-04-06 10:22:28 -0300

    Commit: formating of internal mapping of structureMap to fix json objects

---
+++ Formatting misc.js
@@ -0,0 +1,313 @@
+
+
+export const patente = "patente"  ;
+export const numeroDeChasis = "numeroDeChasis"  ;
+export const numeroMotor = "numeroMotor"  ;
+export const marcaDelVehículo = "marcaDelVehículo"  ;
+export const añoDeFabricación = "añoDeFabricación"  ;
+export const subModelo = "subModelo"  ;
+export const modelo = "modelo"  ;
+export const kilómetros = "kilómetros"  ;
+export const tipoVehiculo = "tipoVehiculo"  ;
+export const combustible = "combustible"  ;
+export const cantidadDeEjes = "cantidadDeEjes"  ;
+export const apellido = "apellido"  ;
+export const nombre = "nombre"  ;
+export const dirección = "dirección"  ;
+export const documentoDeIdentidad = "documentoDeIdentidad"  ;
+export const email = "email"  ;
+export const teléfono = "teléfono"  ;
+export const localidad = "localidad"  ;
+export const provincia = "provincia"  ;
+export const idPrueba = "idPrueba"  ; // ID PRUEBA (Referencia Tramite)
+export const fechaHora = "fechaHora"  ; // (AAAAMMDDhhmmss)
+export const pruebaDeAlineación = "pruebaDeAlineación"  ;
+export const pruebaDeSuspension = "pruebaDeSuspension"  ;
+export const pruebaDeFrenos = "pruebaDeFrenos"  ;
+export const pruebaDeFrenosDeMano = "pruebaDeFrenosDeMano"  ;
+export const pruebaDeLuces = "pruebaDeLuces"  ;
+export const pruebaDeGases = "pruebaDeGases"  ;
+export const pruebaDeInspVisual = "pruebaDeInspVisual"  ;
+export const pruebaDeInspFosa = "pruebaDeInspFosa"  ;
+export const pruebaSonometro = "pruebaSonometro"  ;
+
+
+export const resultadoAlineaciónEje1 = "resultadoAlineaciónEje1"  ;
+export const resultadoAlineaciónEje2 = "resultadoAlineaciónEje2"  ;
+export const resultadoAlineaciónEje3 = "resultadoAlineaciónEje3"  ;
+export const resultadoAlineaciónEje4 = "resultadoAlineaciónEje4"  ;
+export const resultadoAlineaciónEje5 = "resultadoAlineaciónEje5"  ;
+export const resultadoAlineaciónEje6 = "resultadoAlineaciónEje6"  ;
+
+
+
+
+export const rendimientoDelanteroDerecho = "rendimientoDelanteroDerecho"  ;
+export const rendimientoDelanteroIzquierdo = "rendimientoDelanteroIzquierdo"  ;
+export const amplitudMáximaLadoDerecho = "amplitudMáximaLadoDerecho"  ;
+export const amplitudMáximoLadoIzquierdo = "amplitudMáximoLadoIzquierdo"  ;
+export const pesoLadoDerecho = "pesoLadoDerecho"  ;
+export const pesoLadoIzquierdo = "pesoLadoIzquierdo"  ;
+export const diferenciaRendimientoLadoALado = "diferenciaRendimientoLadoALado"  ;
+
+
+
+
+/* export const varible = "Rendimiento Delantero Derecho"  ;
+export const varible = "Rendimiento Delantero Izquierdo"  ;
+export const varible = "Amplitud Máxima Lado Derecho"  ;
+export const varible = "Amplitud Máximo Lado Izquierdo"  ;
+export const varible = "Peso Lado Derecho"  ;
+export const varible = "Peso Lado Izquierdo"  ;
+export const varible = "Diferencia Rendimiento Lado A Lado"  ; */
+
+
+export const resultadoMediciónOpacidad = "resultadoMediciónOpacidad"  ;
+
+
+export const varible = "ResultadoMonoxidoDeCarbonoCO"  ;
+export const varible = "ResultadoPartesPorMillónHC"  ;
+export const varible = "ResultadoSensorDeOxigenoO2"  ;
+export const varible = "ResultadoPartesPorMillónNox"  ;
+export const varible = "ResultadoDioxidoDeCarbonoCO1"  ;
+export const varible = "Lambda"  ;
+
+
+export const varible = "fuerzaDeFrenadoLadoDerecho"  ;
+export const varible = "FuerzaDeFrenadoLadoIzquierdo"  ;
+export const varible = "DiferenciaFuerzaFrenadoLadoALado"  ;
+export const varible = "OvalidadLadoDerecho"  ;
+export const varible = "OvalidadLadoIzquierdo"  ;
+export const varible = "ResistenciaLadoDerecho"  ;
+export const varible = "ResistenciaLadoIzquierdo"  ;
+export const varible = "rendimientoDelEje"  ;
+export const varible = "PesoLadoDerecho"  ;
+export const varible = "PesoLadoIzquierdo"  ;
+
+
+
+
+export const varible = "fuerzaDeFrenadoLadoDerecho"  ;
+export const varible = "FuerzaDeFrenadoLadoizquierdo"  ;
+export const varible = "DiferenciaFzaFrenadoLado A Lado"  ;
+export const varible = "OvalidadLadoDerecho"  ;
+export const varible = "OvalidadLadoIzquierdo"  ;
+export const varible = "ResistenciaLadoDerecho"  ;
+export const varible = "ResistenciaALaRodaduraLadoIzquierdo"  ;
+export const varible = "rendimientoDelEje"  ;
+export const varible = "PesoLadoDerecho"  ;
+export const varible = "PesoLadoIzquierdo"  ;
+
+
+
+
+export const varible = "fuerzaDeFrenadoLadoDerecho"  ;
+export const varible = "FuerzaDeFrenadoLadoIzquierdo"  ;
+export const varible = "DiferenciaFzaFrenadoLadoALado"  ;
+export const varible = "OvalidadLadoDerecho"  ;
+export const varible = "OvalidadLadoIzquierdo"  ;
+export const varible = "ResistenciaLadoDerecho"  ;
+export const varible = "ResistenciaLadoIzquierdo"  ;
+export const varible = "rendimientoDelEje"  ;
+export const varible = "PesoLadoDerecho"  ;
+export const varible = "PesoLadoIzquierdo"  ;
+
+
+
+
+export const varible = "fuerzaDeFrenadoLadoDerecho"  ;
+export const varible = "FuerzaDeFrenadoLadoIzquierdo"  ;
+export const varible = "DiferenciaFzaFrenadoLadoALado"  ;
+export const varible = "OvalidadLadoDerecho"  ;
+export const varible = "OvalidadLadoIzquierdo"  ;
+export const varible = "ResistenciaLadoDerecho"  ;
+export const varible = "ResistenciaLadoIzquierdo"  ;
+export const varible = "rendimientoDelEje"  ;
+export const varible = "PesoLadoDerecho"  ;
+export const varible = "PesoLadoIzquierdo"  ;
+
+
+
+
+export const varible = "fuerzaDeFrenadoLadoDerecho"  ;
+export const varible = "FuerzaDeFrenadoLadoIzquierdo"  ;
+export const varible = "DiferenciaFzaFrenadoLadoALado"  ;
+export const varible = "OvalidadLadoDerecho"  ;
+export const varible = "OvalidadLadoIzquierdo"  ;
+export const varible = "ResistenciaLadoDerecho"  ;
+export const varible = "ResistenciaLadoIzquierdo"  ;
+export const varible = "rendimientoDelEje"  ;
+export const varible = "PesoLadoDerecho"  ;
+export const varible = "PesoLadoIzquierdo"  ;
+
+
+
+
+export const varible = "fuerzaDeFrenadoLadoDerecho"  ;
+export const varible = "FuerzaDeFrenadoLadoIzquierdo"  ;
+export const varible = "DiferenciaFzaFrenadoLadoALado"  ;
+export const varible = "OvalidadLadoDerecho"  ;
+export const varible = "OvalidadLadoIzquierdo"  ;
+export const varible = "ResistenciaLadoDerecho"  ;
+export const varible = "ResistenciaLadoIzquierdo"  ;
+export const varible = "rendimientoDelEje"  ;
+export const varible = "PesoLadoDerecho"  ;
+export const varible = "PesoLadoIzquierdo"  ;
+
+
+
+
+export const varible = "fuerzaDeFrenadoLadoDerecho"  ;
+export const varible = "FuerzaDeFrenadoLadoIzquierdo"  ;
+export const varible = "DiferenciaFzaFrenadoLadoALado"  ;
+export const varible = "rendimientoDelEje"  ;
+export const varible = "Ovalidad Lado Derecho"  ;
+export const varible = "Ovalidad Lado Izquierdo"  ;
+export const varible = "Resistencia Lado Derecho"  ;
+export const varible = "Resistencia Lado Izquierdo"  ;
+
+
+
+
+export const varible = "fuerzaDeFrenadoLadoDerecho"  ;
+export const varible = "Fuerza De Frenado Lado Izquierdo"  ;
+export const varible = "Diferencia Fza. Frenado Lado A Lado"  ;
+export const varible = "rendimientoDelEje"  ;
+export const varible = "Ovalidad Lado Derecho"  ;
+export const varible = "Ovalidad Lado Izquierdo"  ;
+export const varible = "Resistencia Lado Derecho"  ;
+export const varible = "Resistencia Lado Izquierdo"  ;
+
+
+
+
+export const varible = "fuerzaDeFrenadoLadoDerecho"  ;
+export const varible = "Fuerza De Frenado Lado Izquierdo"  ;
+export const varible = "Diferencia Fza. Frenado Lado A Lado"  ;
+export const varible = "rendimientoDelEje"  ;
+export const varible = "Ovalidad Lado Derecho"  ;
+export const varible = "Ovalidad Lado Izquierdo"  ;
+export const varible = "Resistencia Lado Derecho"  ;
+export const varible = "Resistencia Lado Izquierdo"  ;
+
+
+
+
+export const varible = "fuerzaDeFrenadoLadoDerecho"  ;
+export const varible = "Fuerza De Frenado Lado Izquierdo"  ;
+export const varible = "Diferencia Fuerza Frenado Lado A Lado"  ;
+export const varible = "rendimientoDelEje"  ;
+export const varible = "Ovalidad Lado Derecho"  ;
+export const varible = "Ovalidad Lado Izquierdo"  ;
+export const varible = "Resistencia A La Rodadura Lado Derecho"  ;
+export const varible = "Resistencia A La Rodadura Lado Izquierdo"  ;
+
+
+
+
+export const varible = "fuerzaDeFrenadoLadoDerecho"  ;
+export const varible = "Fuerza De Frenado Lado Izquierdo"  ;
+export const varible = "Diferencia Fza. Frenado Lado A Lado"  ;
+export const varible = "rendimientoDelEje"  ;
+export const varible = "Ovalidad Lado Derecho"  ;
+export const varible = "Ovalidad Lado Izquierdo"  ;
+export const varible = "Resistencia A La Rodadura Lado Derecho"  ;
+export const varible = "Resistencia A La Rodadura Lado Izquierdo"  ;
+
+
+
+
+export const varible = "fuerzaDeFrenadoLadoDerecho"  ;
+export const varible = "Fuerza De Frenado Lado Izquierdo"  ;
+export const varible = "Diferencia Fuerza Frenado Lado A Lado"  ;
+export const varible = "rendimientoDelEje"  ;
+export const varible = "Ovalidad Lado Derecho"  ;
+export const varible = "Ovalidad Lado Izquierdo"  ;
+export const varible = "Resistencia A La Rodadura Lado Derecho"  ;
+export const varible = "Resistencia A La Rodadura Lado Izquierdo"  ;
+
+
+
+
+export const varible = "Alineación Faro Derecho Horizontal"  ;
+export const varible = "Alineación Faro Izquierdo Horizontal"  ;
+export const varible = "Intensidad Alta Derecha"  ;
+export const varible = "Intensidad Alta Izquierda"  ;
+export const varible = "Intensidad Baja Derecha"  ;
+export const varible = "Intensidad Baja Izquierda"  ;
+export const varible = "Intensidad Auxiliar Derecho"  ;
+export const varible = "Intensidad Auxiliar Izquierdo"  ;
+export const varible = "Alineación Faro Derecho Vertical"  ;
+export const varible = "Alineación Faro Izquierdo Vertical"  ;
+
+
+
+
+export const varible = "Motivo De Corte De Prueba De Freno Eje 1"  ;
+export const varible = "Motivo De Corte De Prueba De Freno Eje 2"  ;
+export const varible = "Motivo De Corte De Prueba De Freno Eje 3"  ;
+export const varible = "Motivo De Corte De Prueba De Freno Eje 4"  ;
+export const varible = "Motivo De Corte De Prueba De Freno Eje 5"  ;
+export const varible = "Motivo De Corte De Prueba De Freno Eje 6"  ;
+export const varible = "Motivo De Corte De Prueba De F. Mano Eje 1"  ;
+export const varible = "Motivo De Corte De Prueba De F. Mano Eje 2"  ;
+export const varible = "Motivo De Corte De Prueba De F. Mano Eje 3"  ;
+export const varible = "Motivo De Corte De Prueba De F. Mano Eje 4"  ;
+export const varible = "Motivo De Corte De Prueba De F. Mano Eje 5"  ;
+export const varible = "Motivo De Corte De Prueba De F. Mano Eje 6"  ;
+
+
+
+
+export const varible = "Valor De Cero De BF Derecho"  ;
+export const varible = "Valor De Cero De BF Izquierdo"  ;
+export const varible = "Valor De Cero Balanza Derecha"  ;
+export const varible = "Valor De Cero De Balanza Izquierda"  ;
+export const varible = "Valor De Ganancia BF Izquierdo"  ;
+export const varible = "Valor De Ganancia De BF Derecho"  ;
+export const varible = "Valor De Ganancia De Balanza Izquierda"  ;
+export const varible = "Valor De Ganancia De Balanza Derecha"  ;
+
+
+
+
+export const varible = "Fecha Ingreso Al Puesto 1"  ;
+export const varible = "Fecha De Salida Del Puesto 1"  ;
+export const varible = "Operador Puesto 1"  ;
+export const varible = "Fecha Ingreso Al Puesto 2"  ;
+export const varible = "Fecha De Salida Del Puesto 2"  ;
+export const varible = "Operador Puesto 2"  ;
+export const varible = "Fecha Ingreso Al Puesto 3"  ;
+export const varible = "Fecha De Salida Del Puesto 3"  ;
+export const varible = "Operador Puesto 3"  ;
+export const varible = "Fecha Ingreso Al Puesto 4"  ;
+export const varible = "Fecha De Salida Del Puesto 4"  ;
+export const varible = "Operador Puesto 4"  ;
+export const varible = "Fecha Ingreso Al Puesto 5"  ;
+export const varible = "Fecha De Salida Del Puesto 5"  ;
+export const varible = "Operador Puesto 5"  ;
+export const varible = "Fecha Ingreso Al Puesto 6"  ;
+export const varible = "Fecha De Salida Del Puesto 6"  ;
+export const varible = "Operador Puesto 6"  ;
+export const varible = "Fecha Ingreso Al Puesto 7"  ;
+export const varible = "Fecha De Salida Del Puesto 7"  ;
+export const varible = "Operador Puesto 7"  ;
+export const varible = "Fecha Ingreso Al Puesto 8"  ;
+export const varible = "Fecha De Salida Del Puesto 8"  ;
+export const varible = "Operador Puesto 8"  ;
+export const varible = "Fecha Ingreso Al Puesto 9"  ;
+export const varible = "Fecha De Salida Del Puesto 9"  ;
+export const varible = "Operador Puesto 9"  ;
+export const varible = "Fecha Ingreso Al Puesto 10"  ;
+export const varible = "Fecha De Salida Del Puesto 10"  ;
+export const varible = "Operador Puesto 10"  ;
+export const varible = "Nro De Línea Pto1"  ;
+export const varible = "Nro De Línea Pto2"  ;
+export const varible = "Nro De Línea Pto3"  ;
+export const varible = "Nro De Línea Pto4"  ;
+export const varible = "Nro De Línea Pto5"  ;
+export const varible = "Nro De Línea Pto6"  ;
+export const varible = "Nro De Línea Pto7"  ;
+export const varible = "Nro De Línea Pto8"  ;
+export const varible = "Nro De Línea Pto9"  ;
+export const varible = "Nro De Línea Pto10"  ;
--- Functional/constantVariables.js
+++ Functional/constantVariables.js
@@ -129,106 +129,106 @@ export const LINEA_DE_PRUEBA = "lineaDePrueba";
 export const ESTADISTICA_DE_PUESTOS = "estadísticaDePuestos";
 
 /**
- * Identifier for selecction of map in collection of Arrays
+ * Identifier for selecction of mapHeader in collection of Arrays (collectionOfMaps) in fileAnalizer.js Around line 355
  */
 export const intHeader = 0;
 /**
- * Identifier for selecction of map in collection of Arrays
+ * Identifier for selecction of mapAlineacion in collection of Arrays (collectionOfMaps) in fileAnalizer.js Around line 355
  */
 export const intAlineacion = 1;
 /**
- * Identifier for selecction of map in collection of Arrays
+ * Identifier for selecction of mapSuspencionEjeDelantero in collection of Arrays (collectionOfMaps) in fileAnalizer.js Around line 355
  */
 export const intSuspencionEjeDelantero = 2;
 /**
- * Identifier for selecction of map in collection of Arrays
+ * Identifier for selecction of mapSuspencionEjeTrasero in collection of Arrays (collectionOfMaps) in fileAnalizer.js Around line 355
  */
 export const intSuspencionEjeTrasero = 3;
 /**
- * Identifier for selecction of map in collection of Arrays
+ * Identifier for selecction of mapOpacimetro in collection of Arrays (collectionOfMaps) in fileAnalizer.js Around line 355
  */
 export const intOpacimetro = 4;
 /**
- * Identifier for selecction of map in collection of Arrays
+ * Identifier for selecction of mapAnalizadorDeGases in collection of Arrays (collectionOfMaps) in fileAnalizer.js Around line 355
  */
 export const intAnalizadorDeGases = 5;
 /**
- * Identifier for selecction of map in collection of Arrays
+ * Identifier for selecction of mapFrenosEje_1 in collection of Arrays (collectionOfMaps) in fileAnalizer.js Around line 355
  */
 export const intFrenosEje_1 = 6;
 /**
- * Identifier for selecction of map in collection of Arrays
+ * Identifier for selecction of mapFrenosEje_2 in collection of Arrays (collectionOfMaps) in fileAnalizer.js Around line 355
  */
 export const intFrenosEje_2 = 7;
 /**
- * Identifier for selecction of map in collection of Arrays
+ * Identifier for selecction of mapFrenosEje_3 in collection of Arrays (collectionOfMaps) in fileAnalizer.js Around line 355
  */
 export const intFrenosEje_3 = 8;
 /**
- * Identifier for selecction of map in collection of Arrays
+ * Identifier for selecction of mapFrenosEje_4 in collection of Arrays (collectionOfMaps) in fileAnalizer.js Around line 355
  */
 export const intFrenosEje_4 = 9;
 /**
- * Identifier for selecction of map in collection of Arrays
+ * Identifier for selecction of mapFrenosEje_5 in collection of Arrays (collectionOfMaps) in fileAnalizer.js Around line 355
  */
 export const intFrenosEje_5 = 10;
 /**
- * Identifier for selecction of map in collection of Arrays
+ * Identifier for selecction of mapFrenosEje_6 in collection of Arrays (collectionOfMaps) in fileAnalizer.js Around line 355
  */
 export const intFrenosEje_6 = 11;
 /**
- * Identifier for selecction of map in collection of Arrays
+ * Identifier for selecction of mapFrenoDeManoEje_1 in collection of Arrays (collectionOfMaps) in fileAnalizer.js Around line 355
  */
 export const intFrenoDeManoEje_1 = 12;
 /**
- * Identifier for selecction of map in collection of Arrays
+ * Identifier for selecction of mapFrenoDeManoEje_2 in collection of Arrays (collectionOfMaps) in fileAnalizer.js Around line 355
  */
 export const intFrenoDeManoEje_2 = 13;
 /**
- * Identifier for selecction of map in collection of Arrays
+ * Identifier for selecction of mapFrenoDeManoEje_3 in collection of Arrays (collectionOfMaps) in fileAnalizer.js Around line 355
  */
 export const intFrenoDeManoEje_3 = 14;
 /**
- * Identifier for selecction of map in collection of Arrays
+ * Identifier for selecction of mapFrenoDeManoEje_4 in collection of Arrays (collectionOfMaps) in fileAnalizer.js Around line 355
  */
 export const intFrenoDeManoEje_4 = 15;
 /**
- * Identifier for selecction of map in collection of Arrays
+ * Identifier for selecction of mapFrenoDeManoEje_5 in collection of Arrays (collectionOfMaps) in fileAnalizer.js Around line 355
  */
 export const intFrenoDeManoEje_5 = 16;
 /**
- * Identifier for selecction of map in collection of Arrays
+ * Identifier for selecction of mapFrenoDeManoEje_6 in collection of Arrays (collectionOfMaps) in fileAnalizer.js Around line 355
  */
 export const intFrenoDeManoEje_6 = 17;
 /**
- * Identifier for selecction of map in collection of Arrays
+ * Identifier for selecction of mapSonometro in collection of Arrays (collectionOfMaps) in fileAnalizer.js Around line 355
  */
 export const intSonometro = 18;
 /**
- * Identifier for selecction of map in collection of Arrays
+ * Identifier for selecction of mapLuxometro in collection of Arrays (collectionOfMaps) in fileAnalizer.js Around line 355
  */
 export const intLuxometro = 19;
 /**
- * Identifier for selecction of map in collection of Arrays
+ * Identifier for selecction of mapInspeccionVisual in collection of Arrays (collectionOfMaps) in fileAnalizer.js Around line 355
  */
 export const intInspeccionVisual = 20;
 /**
- * Identifier for selecction of map in collection of Arrays
+ * Identifier for selecction of mapInspeccionDH in collection of Arrays (collectionOfMaps) in fileAnalizer.js Around line 355
  */
 export const intInspeccionDH = 21;
 /**
- * Identifier for selecction of map in collection of Arrays
+ * Identifier for selecction of mapFrenos in collection of Arrays (collectionOfMaps) in fileAnalizer.js Around line 355
  */
 export const intFrenos = 22;
 /**
- * Identifier for selecction of map in collection of Arrays
+ * Identifier for selecction of mapValoresCalibrados in collection of Arrays (collectionOfMaps) in fileAnalizer.js Around line 355
  */
 export const intValoresCalibrados = 23;
 /**
- * Identifier for selecction of map in collection of Arrays
+ * Identifier for selecction of mapLineaDePrueba in collection of Arrays (collectionOfMaps) in fileAnalizer.js Around line 355
  */
 export const intLineaDePrueba = 24;
 /**
- * Identifier for selecction of map in collection of Arrays
+ * Identifier for selecction of mapEstadisticaDePuesto in collection of Arrays (collectionOfMaps) in fileAnalizer.js Around line 355
  */
 export const intEstadisticaDePuesto = 25;
